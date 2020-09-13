fc5b678a5065311efa38e9a6a8b14428fefab8e1
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2020-09-14 00:28:30 +0200

    Add nodemailer and relevant links

--- api/getContactForm.ts
+++ api/getContactForm.ts
@@ -1,6 +1,10 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-unused-vars */
 
+// https://nodemailer.com/about/
+// https://www.youtube.com/watch?v=nF9g1825mwk&app=desktop
+// https://www.youtube.com/watch?v=vrbyaOoZ-4Q&feature=youtu.be
+
 import { NowRequest, NowResponse } from '@vercel/node';
 
 export default function processContactForm(req: NowRequest, res: NowResponse) {
